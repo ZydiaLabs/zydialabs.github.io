f94820aa43a4a9039cfb5758a0ee5eb428a0b84c
Giovanny Andres Gongora Granada <gioyik@gmail.com>
2015-02-12 10:25:32 -0500

    template definition and clean

--- scripts/theme.script.js
+++ scripts/theme.script.js
@@ -1,47 +1,6 @@
-/*
-
-	0. PAGE PRELOADER
-	1. HEADER
-	2. NAVIGATION
-	4. WORK
-	5. BLOG
-	6. TOGGLE BOX (SHORTCODES)
-	7. SKILLBAR (SHORTCODES)
-    8. TAB CONTENT (SHORTCODES)
-	9. ALERT BOX (SHORTCODES)
-	10. CLEAR INPUNT
-
-*/
-
 (function ($) {
-
     'use strict';
 	
-	
-    /* --------------------------------------------------------------------- */
-    /* 0. PAGE PRELOADER
-    /* --------------------------------------------------------------------- */
-
-    var windowheight    = $(window).height();
-    var windowWidth     = $(window).width();
-
-    $(window).resize(function(){
-        windowheight    = $(window).height();
-        windowWidth     = $(window).width();
-    });
-
-
-	$(window).load(function(){
-		$("#page-preloader").fadeOut(300);
-	});
-	
-	
-	
-
-    /* --------------------------------------------------------------------- */
-    /* 1. HEADER
-    /* --------------------------------------------------------------------- */
-
     var navistatus = 0;
 
     $("#open-nav").click(function(){
@@ -76,75 +35,13 @@
         return false;
     });
 
-
-
-
-    /* --------------------------------------------------------------------- */
-    /* 2. NAVIGATION
-    /* --------------------------------------------------------------------- */
-
-    $("#main-navigation li a").click(function(){
-        var topoffset = 0;
-        $('html, body').clearQueue().animate({
-            scrollTop: $( $.attr(this, 'href') ).offset().top - topoffset
-        }, 1800,'swing');
-        return false;
-    });
-	
-
-    var mainNav = $("#main-navigation"),
-        mainItems = mainNav.find("a"),
-        scrollTo = mainItems.map(function(){
-            var item = $( $( this ).attr( "href" ) );
-            if (item.length) { return item; }
-        });
-
-    $( window ).scroll( function () {
-
-        var mainHeight = mainNav.height();
-        var offsetTop = $( this ).scrollTop() + mainHeight + 200;
-
-        var current = scrollTo.map( function() {
-            if( $( this ).offset().top < offsetTop )
-                return this;
-        } );
-
-        if( current.length ) {
-            current = current[ current.length -1 ];
-            var id = current.attr( "id" );
-
-        } else {
-            var id = "";
-        }
-
-        mainItems
-            .removeClass( "active" )
-            .filter( "[href=#"+id+"]" )
-            .addClass( "active" );
-    });
-
-
-
-
-    /* --------------------------------------------------------------------- */
-    /* 3. BANNER
-    /* --------------------------------------------------------------------- */
-
-	function sizeBanner()
-	{
+	function sizeBanner(){
 		var windowHeight = $(window).height();
 		$("#banner").css("height",windowHeight+"px");
 	}
 	$(window).resize(function(){
 		sizeBanner()
 	});
-
-
-
-
-    /* --------------------------------------------------------------------- */
-    /* 4. WORK
-    /* --------------------------------------------------------------------- */
 
     var workWrapper = $("#work-wrapper");
     var workLoader  = $("#work-loader");
@@ -164,8 +61,6 @@
         workArticle.css("minHeight",windowheight+"px");
         workArticle.fadeIn(1000);
         workWrapper.css("height","auto");
-
-        /* === FUNCTION TO DO AFTER LOADED THE WORK DETAILS === */
 
         $('.work-slider').bxSlider({
             slideWidth: 1600,
@@ -215,10 +110,6 @@
         }
     });
 
-
-
-    /* === START MIXITUP PLUGIN === */
-
     var pfPreview = $( '#work-preview' );
     var pfTabs = $( '.work-tabs' );
     var pfMixed = 0;
@@ -246,221 +137,6 @@
 
     startmixitup();
 
-
-
-    /* === LOAD MORE WORK === */
-
-    var loadMoreWork = 1;
-    var loadingPortImg = $(".work-button .button img");
-
-    function pad (str, max) {
-        str = str.toString();
-        return str.length < max ? pad("0" + str, max) : str;
-    }
-
-
-    $( "#work-load-more a" ).click( function( e ) {
-
-        loadingPortImg.fadeIn(300);
-        e.preventDefault();
-
-        var active = $('.work-tabs > li.active' ).data( 'filter' );
-        var load = pad( loadMoreWork, 2 );
-
-		var protocol = $(location).attr('protocol');
-
-		if( protocol == 'file:') {
-			alert('"The Buttons "Load More" not working if you open the index.html by double click and have a URL like file://../../index.html\n\nRun it on your localhost or online server and it will work.\n\nReason:\nBrowsers implement strong security measures to prevent downloaded web pages from accessing arbitrary files on the file system.');
-		}
-
-        $.get('work/load-more-' + load + '.html', function(data){
-
-            $(data).appendTo("#work-preview");
-
-            $( '#work-preview > li' ).each( function() { $(this).hoverdir(); } );
-
-            startmixitup();
-
-        }).done(function(){
-            loadingPortImg.fadeOut(300);
-        });
-
-        loadMoreWork = loadMoreWork + 1;
-        load = pad( loadMoreWork, 2 );
-
-        $.get( 'work/load-more-' + load + '.html' ).fail(function() {
-            $( "#work-load-more" ).hide();
-        });
-    } );
-
-
-    /* --------------------------------------------------------------------- */
-    /* 5. BLOG
-    /* --------------------------------------------------------------------- */
-
-    var minWidth    = 965;
-
-    function setBlog()
-    {
-        if(windowWidth < minWidth)
-        {
-           $("#blog-preview li .layer").css({
-                top: '25%',
-                left: '0',
-                width: '100%',
-                opacity: '1'
-           });
-        } else {
-        $("#blog-preview li .layer").css({
-            top: '40%',
-            left: '-50%',
-            width: '50%',
-            opacity: '0'
-        });
-        }
-    }
-
-    setBlog();
-    $(window).resize(function(){
-        setBlog();
-    });
-
-
-    var blog            = $('#blog');
-    var blogWrapper     = $("#blog-wrapper");
-    var closeWrapper    = $("#close-blog-wrapper");
-    var blogLoader      = $("#blog-loader");
-    var loadBlogContent = $("#load-blog-content");
-
-
-    blog.on( 'mouseenter', "#blog-preview li", function(){
-        if(windowWidth > minWidth)
-        {
-            $(this).children(".layer").clearQueue().animate({
-                left: '0',
-                opacity: '1'
-            },400);
-        }
-    });
-
-    blog.on( 'mouseleave', "#blog-preview li", function(){
-		if(windowWidth > minWidth)
-		{
-			$(this).children(".layer").clearQueue().animate({
-				left: '-50%',
-				opacity: '0'
-			},400);
-		}
-	});
-
-
-
-    /* === LOAD BLOG DETAILS === */
-
-    closeWrapper.click(function(){
-        blogWrapper.slideUp(1000);
-        $(".blog-article").fadeOut(800);
-    });
-
-    function doafterloadblog()
-    {
-        var blogArticle = $(".blog-article");
-
-        blogLoader.css("display","none");
-        blogWrapper.children(".close").fadeIn(800);
-        blogArticle.css("minHeight",windowheight+"px");
-        blogArticle.fadeIn(1000);
-        blogWrapper.css("height","auto");
-
-        /* === FUNCTION TO DO AFTER LOADED THE WORK DETAILS === */
-
-        $('.blog-slider').bxSlider({
-            slideWidth: 1600,
-            minSlides: 1,
-            maxSlides: 1,
-			video: true
-        });
-    }
-
-    blog.on( 'click' , "#blog-preview li a", function(){
-
-        var folder  = 'blog/',
-            hash    = $(this).attr('data-blog-link'),
-            url     = hash.replace(/[#\#]/g, '' );
-
-        if(blogWrapper.is(":hidden"))
-        {
-            blogWrapper.children(".close").hide();
-            blogLoader.css("display","block");
-            loadBlogContent.html("");
-            blogWrapper.css("height",windowheight+"px");
-            blogWrapper.slideDown(1000);
-
-            $('html, body').clearQueue().animate({
-                scrollTop: blogWrapper.offset().top
-            },1400,function(){
-
-                loadBlogContent.load(folder+url, function(){
-                    doafterloadblog();
-                });
-            });
-        } else {
-            var getwrapperheight = blogWrapper.height();
-            blogWrapper.css("height",getwrapperheight+"px");
-            blogWrapper.children(".close").fadeOut(800);
-            $(".blog-article").fadeOut(800);
-            blogLoader.delay(850).fadeIn();
-
-            $('html, body').clearQueue().animate({
-                scrollTop: blogWrapper.offset().top
-            },1800,function(){
-
-                loadBlogContent.load(folder+url, function(){
-                    doafterloadblog();
-                });
-            });
-        }
-    });
-
-	var loadMoreBlog = 1;
-    var loadingBlogImg = $(".blog-button .button img");
-
-
-	$( "#blog-load-more a" ).click( function( e ) {
-
-        loadingBlogImg.fadeIn(300);
-		e.preventDefault();
-
-		var load = pad( loadMoreBlog, 2 );
-
-		var protocol = $(location).attr('protocol');
-
-		if( protocol == 'file:') {
-			alert('"The Buttons "Load More" not working if you open the index.html by double click and have a URL like file://../../index.html\n\nRun it on your localhost or online server and it will work.\n\nReason:\nBrowsers implement strong security measures to prevent downloaded web pages from accessing arbitrary files on the file system.');
-		}
-
-
-		$.get('blog/load-more-' + load + '.html', function(data){
-			$(data).appendTo( "#blog-preview" );
-			setBlog();
-		}).done(function(){
-            loadingBlogImg.fadeOut(300);
-        });
-
-		loadMoreBlog = loadMoreBlog + 1;
-		load = pad( loadMoreBlog, 2 );
-
-		$.get( 'blog/load-more-' + load + '.html' ).fail(function() {
-			$( "#blog-load-more" ).hide();
-		});
-	} );
-
-
-
-    /* --------------------------------------------------------------------- */
-    /* 6. TOGGLE BOX (SHORTCODES)
-    /* --------------------------------------------------------------------- */
-	
 	$(".toggle-box .toggle .header").click(function(){
 		if($(this).parent(".toggle").children(".content").is(":hidden"))
 		{
@@ -474,81 +150,6 @@
 			$(this).parent(".toggle").children(".header").children(".plus").css({ display: 'inline-block' });
 			$(this).parent(".toggle").children(".header").children(".minus").css({ display: 'none' });
 		}
-	});
-	
-	
-	
-	
-    /* --------------------------------------------------------------------- */
-    /* 7. SKILLBAR (SHORTCODES)
-    /* --------------------------------------------------------------------- */
-	
-	$(".skillbar").each(function(){
-		var percent = $(this).attr("data-percent");
-		$(this).children(".percent-bg").css("width",percent+"%");
-	});
-
-
-
-
-    /* --------------------------------------------------------------------- */
-    /* 8. TAB CONTENT (SHORTCODES)
-     /* --------------------------------------------------------------------- */
-
-    $(".tab-content-navi").each(function(){
-        var link = $(this).children("li:first-child").children("a");
-        var contentId = link.attr("data-open-content");
-        $("#"+contentId).css("display","block");
-        link.addClass("active");
-    });
-
-    $(".tab-content-navi li a").click(function(){
-        var contentId = $(this).attr("data-open-content");
-
-        if($("#"+contentId).is(":hidden"))
-        {
-            var list = $(this).parent("li").parent("ul");
-            list.children("li").each(function(){
-                $(this).children("a").removeClass("active");
-                var contentId = $(this).children("a").attr("data-open-content");
-                $("#"+contentId).css("display","none");
-            });
-            $("#"+contentId).fadeIn(400);
-            $(this).addClass("active");
-        }
-    });
-	
-	
-	
-	
-    /* --------------------------------------------------------------------- */
-    /* 9. ALERT BOX
-    /* --------------------------------------------------------------------- */
-	
-	$(".alert-box .close").click(function(){
-		$(this).parent(".alert-box ").fadeOut(350);
-	});
-
-
-
-
-    /* --------------------------------------------------------------------- */
-    /* 10. CLEAR INPUNT
-    /* --------------------------------------------------------------------- */
-
-    var defaultvalue;
-    $(".cleartext").focusin(function() {
-        defaultvalue = this.defaultValue;
-        if(this.value == this.defaultValue) {
-            this.value = '';
-        }
-    }).focusout(function(){
-        if($(this).val() == '')
-        {
-            this.value = defaultvalue;
-        }
-    });
-
+	});	
 
 })(jQuery);
-
--- scripts/theme.settings.js
+++ scripts/theme.settings.js
@@ -1,37 +1,10 @@
-/*
-
-	1. WORK PREVIEW HOVERDIR
-	2. PARALLAX BACKGROUNDS
-	3. SLIDER
-	  3.0 FULLSCREEN HEADER IMAGE SLIDER
-	  3.1 MACBOOK DEVICE SLIDER
-	  3.2 IPHONE DEVICE SLIDER
-	  3.3 COMMENT SLIDER
-	4. CAROUSEL
-	  4.1 CLIENT CAROUSEL
-    5. GOOGLE MAP
-
-
-*/
-
 (function ($) {
-
     'use strict';
-
-    /* ----------------------------------------- */
-    /* 1. WORK PREVIEW HOVERDIR
-    /* ----------------------------------------- */
 
     $(function() {
         $(' #work-preview > li ').each( function() { $(this).hoverdir(); } );
     });
-
-
-
-    /* ----------------------------------------- */
-    /* 2. PARALLAX BACKGROUNDS
-    /* ----------------------------------------- */
-
+    
     $('#banner').parallax({
         speed :	0.25
     });
@@ -40,33 +13,6 @@
         speed :	0.25
     });
 
-
-
-    /* ----------------------------------------- */
-    /* 3. SLIDER
-    /* ----------------------------------------- */
-	
-    /* ==== 3.0 FULLSCREEN HEADER IMAGE SLIDER  ==== */
-
-    var headerSlider = $('#maximage-slider');
-
-    if(headerSlider.length) // CHECK IF ELEMENT EXISTS
-    {
-        headerSlider.maximage({
-            cycleOptions: {
-                fx: 'fade',
-                speed: 1000,
-                timeout: 3000,
-                prev: '.maximage-prev',
-                next: '.maximage-next',
-                pause: 1
-            }
-        });
-    }
-
-
-    /* ==== 3.1 MACBOOK DEVICE SLIDER  ==== */
-
     $('#macbook-01').bxSlider({
         slideWidth: 1200,
         minSlides: 1,
@@ -74,8 +20,6 @@
         controls: false,
         auto: true
     });
-
-    /* ==== 3.2 IPHONE DEVICE SLIDER  ==== */
 
     $('#iphone-01').bxSlider({
         slideWidth: 1200,
@@ -86,26 +30,7 @@
         autoDelay: 500
     });
 
-    /* ==== 3.3 COMMENT SLIDER  ==== */
-
-    $('#comment-slider').bxSlider({
-        slideWidth: 1200,
-        minSlides: 1,
-        maxSlides: 1,
-        controls: false,
-		touchEnabled: false
-    });
-
-
-
     $(window).load(function(){
-		
-	    /* ----------------------------------------- */
-	    /* 4. CAROUSEL
-	    /* ----------------------------------------- */
-
-	    /* ==== 4.1 CLIENT CAROUSEL  ==== */
-
 	    $('#client-carousel').carouFredSel({
 	        responsive: true,
 	        width: '100%',
@@ -118,23 +43,5 @@
 	            }
 	        }
 	    });
-		
     });
-
-
-
-    /* ----------------------------------------- */
-    /* 5. GOOGLE MAP
-    /* ----------------------------------------- */
-
-    var googleMap = $('#googlemap');
-
-    if(googleMap.length) // CHECK IF ELEMENT EXIST
-    {
-        googleMap.CustomMap();
-    }
-	
-
-
 })(jQuery);
-
